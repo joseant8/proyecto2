3a6ae2ad5b11b3a4b97be7eb5b700267f7eab5fe
Jose Antonio <j2a2mari@gmail.com>
2021-03-14 00:40:35 +0100

    Test announce() method

---
+++ src/test/java/com/example/service/EventNotificationTest.java
@@ -0,0 +1,43 @@
+package com.example.service;
+
+import com.example.Attendee;
+import com.example.Event;
+import com.example.EventType;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EventNotificationTest {
+
+    private Event event;
+    private Attendee attendee;
+    private EventNotificationService eventNotification;
+
+    @BeforeEach
+    public void init(){
+        event = new Event(1L, "Mobile Week", EventType.TECH, null);
+        attendee = new Attendee(1L, "Jose", "jose@email.com");
+        eventNotification = new EventNotificationServiceImpl();
+    }
+
+
+
+    @Test
+    void announce() {
+        assertNotNull(eventNotification);
+
+        // Si el asistente no está en la lista del evento, no tendrá la notificación
+        eventNotification.announce(event);
+        assertTrue(attendee.getNotifications().isEmpty());
+
+        // Si el asistente está en la lista del evento, tendrá la notificación
+        event.addAttendee(attendee);
+        eventNotification.announce(event);
+        assertFalse(attendee.getNotifications().isEmpty());
+    }
+
+    @Test
+    void confirmAttendance() {
+    }
+}
