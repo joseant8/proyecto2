a0350536978f00043aee25a9bfb605bd8112e6b8
Jose Antonio <j2a2mari@gmail.com>
2021-03-14 01:17:26 +0100

    Created EventTest class

---
+++ src/test/java/com/example/EventTest.java
@@ -0,0 +1,79 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class EventTest {
+
+    @Test
+    void addAttendee() {
+    }
+
+    @Test
+    void addAttendees() {
+    }
+
+    @Test
+    void removeAttendee() {
+    }
+
+    @Test
+    void removeAttendees() {
+    }
+
+    @Test
+    void notifyAssistants() {
+    }
+
+    @Test
+    void addSpeaker() {
+    }
+
+    @Test
+    void removeSpeaker() {
+    }
+
+    @Test
+    void getId() {
+    }
+
+    @Test
+    void setId() {
+    }
+
+    @Test
+    void getTitle() {
+    }
+
+    @Test
+    void setTitle() {
+    }
+
+    @Test
+    void getType() {
+    }
+
+    @Test
+    void setType() {
+    }
+
+    @Test
+    void getSpeakers() {
+    }
+
+    @Test
+    void setSpeakers() {
+    }
+
+    @Test
+    void getAttendees() {
+    }
+
+    @Test
+    void setAttendees() {
+    }
+}
